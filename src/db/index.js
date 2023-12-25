ce0c39d7e56a9c4d139366050d219e80e7dcd610
SaiEswar15 <saieswarkumar@gmail.com>
2023-12-26 00:22:48 +0530

    connecting to db

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async ()=>{
+    try
+    {
+        
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}` )
+        console.log("MONGO_DB connection SUCCESSFUL....", `DB HOST : ${connectionInstance.connection.host}`)
+    }
+    catch(error)
+    {
+        console.log("ERROR : MONGO_DB connection FAILED...", error)
+        process.exit(1); //study about this 
+    }
+}
+
