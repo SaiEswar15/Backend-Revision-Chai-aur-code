2962b4be1074e3a263e5a6a1662a49da395c3404
SaiEswar15 <saieswarkumar@gmail.com>
2023-12-28 00:58:59 +0530

    generating refresh token functionality

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -82,6 +82,17 @@ userSchema.methods.generateAccessToken = function()
     )      
 }
 
-
+userSchema.methods.generateRefreshToken = function()
+{
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )      
+}
 
 export const users = mongoose.model("User", userSchema)
