25ff1c0490e80f2a557130647bc09f1c5a544045
SaiEswar15 <saieswarkumar@gmail.com>
2023-12-28 15:06:54 +0530

    cloudinary configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async function(localFilePath)
+{
+    //this uplaod may fail so use try catch
+    try 
+    {
+        if(!localFilePath) return null;
+        const response = await cloudinary.v2.uploader.upload(localFilePath, {resource_type : auto});
+        console.log("file uploaded successfully", response.url);
+        return response;
+    } 
+    catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+
+// we can directly upload like this :
+
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result)
+//   {
+//     console.log(result); 
+//   });
+
+// we can directly upload but in industry there will be two step process as above:
