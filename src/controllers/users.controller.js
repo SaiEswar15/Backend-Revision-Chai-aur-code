fbd13d975594681dad13d12a9768d1ea70c3345b
SaiEswar15 <saieswarkumar@gmail.com>
2023-12-29 18:05:10 +0530

    register user logic with multiple validations

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,21 +1,64 @@
 import { asyncHandlerPromises } from "../utils/asyncHandler.js";
-
+import ApiErrors from "../utils/apiErrors.js";
+import {ApiResponse} from "../utils/apiResponse.js";
+import { users as userSchema } from "../models/users.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const register = asyncHandlerPromises(async (req,res)=>{
     
     //get user details from frontend
     const {username, email, fullName, password } = req.body;
 
-    //check if you are getting the data 
-    console.log("username :",username,"email :", email,"full name :", fullName,"password :", password)
-
-    if(!username || !email || !fullName || !password)
+    //validate if all feilds are present or not 
+    if([ username, email, fullName, password ].some((el)=> el?.trim() === ""))
     {
-        console.log("all feilds required")
+        throw new ApiErrors(400, "All feilds required")
     }
-    res.status(200).json({
-        message : "registered successfully"
+
+    //if user already exists or not 
+    const existUser = await userSchema.findOne({$or : [{email},{username}]})
+
+    if(existUser)
+    {
+        throw new ApiErrors(301, "user already exist")
+    }
+
+    //get user files
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    //no gaurentte if they are present or not - so validation
+    if (!avatarLocalPath)
+    {
+        throw new ApiErrors(409, "problem uploading avatar")
+    }
+
+    //now we have path and we have to  upload to cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    //check if they are uploaded successfully or not 
+    if (!avatar)
+    {
+        throw new ApiErrors(500, "problem uploading avatar in cloudinary")
+    }
+
+    //create object and put in database
+    const user = await userSchema.create({
+        email,
+        password,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || null,
+        username : username.toLowerCase()
+
     })
+
+    //check if user is present or not in db
+    const updatedUser = await userSchema.findById(user._id).select("-password -refreshToken")
+
+    if(!updatedUser) throw new ApiErrors(500, "failed registering user")
+    
+    res.status(200).json(new ApiResponse(200, updatedUser, "user registered successfully"))
 })
 
 const check = asyncHandlerPromises(async (req,res)=>{
