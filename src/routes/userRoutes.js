f5afc15fb77444fea9192e86b6f30ed9fff44b44
SaiEswar15 <saieswarkumar@gmail.com>
2023-12-29 18:01:12 +0530

    configuring multer middleware to route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { check, register } from "../controllers/users.controller.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(register)
+userRouter.route("/register").post( upload.fields(
+    [
+        {
+            name : "avatar", //name of the feild for communication
+            maxCount : 1,
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]
+) , register)
 userRouter.route("/check").get(check)
 
 export {userRouter};
